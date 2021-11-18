5dad12540e56f743957f00b86270102eb700a55b
Darah Snider <darahsnider@gmail.com>
2021-11-18 17:18:10 -0500

    fixed db and sequelize errors (db wasnt in server dir. const sequelize was calling on port 5342 instead of 5432) Through 6.3 complete

--- db.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize("postgres://postgres:7cfa37e368584d0785269f66d3c39c22@localhost:5342/2-workout-log");
-
-module.exports = sequelize;
